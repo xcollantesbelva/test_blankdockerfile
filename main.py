c947f0c7b548ff4139ba4e76ec7075a63bd3c782
Xavier Collantes <xavier.collantes@belva.ai>
2024-08-09 10:39:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 """Dummy app."""
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
-async def get_cat_fact():
-    return {"response": "This is a repo with a bad Dockerfile."}
+@app.get("/", response_class=HTMLResponse)
+async def index():
+    return """
+    <h2>This is a repo with a bad Dockerfile.</h2>
+    <iframe width="860" height="515" src="https://www.youtube.com/embed/VZrDxD0Za9I?si=SqlyK4N3x_3TDQzE"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write;
+        encrypted-media; gyroscope; picture-in-picture;
+        web-share" referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen>
+    </iframe>
+    """
 
 
 if __name__ == "__main__":
